15fff55364b811231ceac2f0f0d36243ab57696f
Daylin Ortega <daylin.ortega@ug.uchile.cl>
2025-10-04 20:05:46 -0300

    Cambié unas cosas en el salto del humano y con la orientación de los psjes

--- waren-studio/personajes/prota.gd
+++ waren-studio/personajes/prota.gd
@@ -1,14 +1,16 @@
 extends CharacterBody2D
 
 #VARIABLES HUMANO (modificar)
-var max_speed = 150
-var jump_speed = 400
-var gravity = 1200
-var acceleration = 500
+@export var max_speed = 250
+@export var jump_speed = 400
+@export var gravity = 1200
+@export var acceleration = 500
 
 @onready var animation_player: AnimationPlayer = $Human/AnimationPlayer
 @onready var animation_tree: AnimationTree = $Human/AnimationTree
 @onready var playback = animation_tree["parameters/playback"]
+@onready var pivot: Node2D = $Human/Pivot
+
 
 func _ready() -> void:
 	pass
@@ -23,7 +25,16 @@ func _physics_process(delta: float) -> void:
 	velocity.x = move_toward(velocity.x, move_input* max_speed, acceleration*delta)
 	move_and_slide()
 	
-	if abs(velocity.x) > 10:
-		playback.travel("human_run")
-	else:
-		playback.travel("human_idle")
+	if move_input:
+		pivot.scale.x = sign(move_input)
+	if is_on_floor():
+		if abs(velocity.x) > 10:
+			playback.travel("human_run")
+		else:
+			playback.travel("human_idle")
+	else: 
+		if velocity.y < 0:
+			playback.travel("human_jump_star")
+		else:
+			playback.travel("human_jump_fall")
+	
--- waren-studio/personajes/rana.gd
+++ waren-studio/personajes/rana.gd
@@ -1,16 +1,18 @@
 extends CharacterBody2D
 
 #VARIABLES RANAA
-var max_speed = 300
-var jump_speed = 500
-var gravity = 800
-var acceleration = 500
+@export var max_speed = 300
+@export var jump_speed = 500
+@export var gravity = 800
+@export var acceleration = 500
 
 var jumping = false
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var playback = animation_tree["parameters/playback"]
+@onready var pivot: Node2D = $Pivot
+
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -21,8 +23,12 @@ func _physics_process(delta: float) -> void:
 	var move_input = Input.get_axis("move_left", "move_right")
 	velocity.x = move_toward(velocity.x, move_input* max_speed, acceleration*delta)
 	move_and_slide()
+	
+	if move_input:
+		pivot.scale.x = sign(move_input)
 
 	if abs(velocity.x) > 10:
 		playback.travel("frog_run")
 	else:
 		playback.travel("frog_idle")
+		
